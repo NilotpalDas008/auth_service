2837e7946c0b7f716bf201b5d2679c5eefe0b308
NilotpalDas008 <nd8851715@gmail.com>
2026-02-12 20:30:42 +0530

    write the route , middleware and controller

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,19 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt.js';
 
+const userSchema =new mongoose.Schema({
+    email:{ type: String, required: true, unique: true, lowercase: true },
+    password:{ type: String, required: true, select: false },
+});
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+export default mongoose.model('User', userSchema);
